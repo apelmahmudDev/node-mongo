af3ab0e19a21e05777e4cd3ea4e7afa9e64b8968
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-08-11 07:29:21 +0800

    Add an API in the last time

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ app.get('/products', (req, res) => {
     });
 });
 
+app.get('/orders', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true});
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("orders");
+        collection.find().toArray((err, documents) => {
+           if(err){
+               console.log(err);
+               res.status(5000).send({message:err});
+           }
+           else{
+                res.send(documents);
+           };
+            client.close();
+        });
+    });
+});
+
 app.get('/product/:key', (req, res) => {
     const key = req.params.key;
 
